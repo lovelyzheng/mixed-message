e10c79a2f86c4cf5c03e8e34916c9c1cc2ee1d96
unknown <lovelyzheng98@gmail.com>
2021-02-08 16:23:09 +0800

    script is done

---
+++ script.js
@@ -0,0 +1,10 @@
+const randomMessage = ["Can't hold me down 'cuz you know I'm a fighter",
+"Don’t be trapped in someone else’s dream","I’m diamond, you know I glow up",
+"Don’t ever be scared. Whatever people say, you’re okay", "Run, run, run again.It’s okay to fall",
+"I'm the one I should love in this world", "I’m feeling just fine, fine, fine",
+"It's okay to shed the tears. But don't you tear yourself","You thought I was gonna fail but I’m fine, sorry",
+"In a world where you feel cold. You gotta stay gold"]
+
+let randonNumber = Math.floor(Math.random()*10)
+
+console.log(randomMessage[randonNumber]);
